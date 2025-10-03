3c92e094cb420dc16e9d25597970c9181c10cc5a
madhan-kumar03 <231901028@rajalakshmi.edu.in>
2025-10-03 11:22:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ import MedicalShop from './components/MedicalShop';
+ import './App.css';
+ function App() {
+    return (
+        <div>
+           <MedicalShop />
+        </div>
+    );
+}
+ export default App;
